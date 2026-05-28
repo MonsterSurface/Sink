dfa6258382298d503baf0c2e82ccd1280cbcedf3
MonsterSurface <96628983+MonsterSurface@users.noreply.github.com>
2026-05-28 11:24:24 +0100

    Add title back and remove github stars

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,8 +4,8 @@ import { Menu, Star, X } from 'lucide-vue-next'
 import { GitHubIcon, TelegramIcon, XIcon } from 'vue3-simple-icons'
 
 const showMenu = ref(false)
-// const { title, telegram, twitter, github } = useAppConfig()
-const { rawStats } = useGithubStats()
+const { title } = useAppConfig()
+//const { rawStats } = useGithubStats()
 </script>
 
 <template>
