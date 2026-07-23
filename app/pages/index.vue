3d64e47d5f91eca3338d28ff40df85d80269e4bb
MonsterSurface <96628983+MonsterSurface@users.noreply.github.com>
2026-07-23 14:09:48 +0100

    Remove Homepage again

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,3 @@
-<template>
-  <div
-    class="
-      flex flex-col justify-center overflow-x-clip bg-background text-foreground
-    "
-  >
-    <HomeHero />
-  </div>
-</template>
+<script setup>
+navigateTo('/dashboard')
+</script>
